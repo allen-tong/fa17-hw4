36c974573f53ded1530b31d225fa054aa9906334
Allen Tong <allentong29@gmail.com>
2017-10-11 07:35:41 -0700

    Completed hw4

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/', to: redirect('/home')
+  get 'home', to: 'home#index', as: 'home'
+  get 'todos/new', to: 'todos#new'
+  post 'todos/create', to: 'todos#create', as: 'create'
 end
